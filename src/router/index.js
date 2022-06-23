29d83fe5aefc11c28e7cb1657a0eef2489a7e09d
Roel Heesterbeek <99894112+REA-Roel@users.noreply.github.com>
2022-06-23 16:19:54 +0200

    reverts master branch to original

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import EventList from '../views/EventList.vue'
-import EventDetails from '../views/EventDetails.vue'
-import About from '../views/About.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'EventList',
-    component: EventList
+    path: "/",
+    name: "Home",
+    component: Home
   },
   {
-    path: '/event/:id',
-    name: 'EventDetails',
-    props: true,
-    component: EventDetails
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
